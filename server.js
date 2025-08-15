aeefa16e9da905f9ed3a8acff2ed608cd9f8932f
Luiz felipe <seuemail@example.com>
2025-08-15 11:49:02 -0300

    bruxos

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import bruxos from "./src/data/bruxos.js";
+
+const app = express();
+
+app.get("/", (req,res) => {
+    res.send("Vamos de Harry Potter")
+})
+
+app.get("/bruxos", (req, res) => {
+    res.json(bruxos);
+});
+
+app.listen(3000, () => {
+    console.log(`Acesse seu site http://localhost:300$`);
+    console.log("🧙‍♂️ API dos Bruxos está no ar na porta 3000 !");
+});
