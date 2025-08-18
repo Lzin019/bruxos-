c5d6b2245652db8776cd705b41629b1f6ae93936
Luiz Felipe <luizfelipeallagedd@gmail.com>
2025-08-18 09:53:37 -0300

    first commit

--- server.js
+++ server.js
@@ -11,7 +11,23 @@ app.get("/bruxos", (req, res) => {
     res.json(bruxos);
 });
 
+
+app.get(`/bruxos/:id`, (req, res) => {
+    let id = req.params.id;
+    id = parseInt(id);
+    const bruxo = bruxos.find(b => b.id === id);
+    
+if (bruxo) {
+    res.json(bruxo);
+} else {
+    res.status(400).json({
+        mensagem: "Bruxo Não encontrado"
+    })
+}
+
+})
+
 app.listen(3000, () => {
-    console.log(`Acesse seu site http://localhost:300$`);
+    console.log(`Acesse seu site http://localhost:3000`);
     console.log("🧙‍♂️ API dos Bruxos está no ar na porta 3000 !");
 });
